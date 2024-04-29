d34f45bd755b344e9dc0c598c31e11722f4bd424
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-04-29 14:23:50 +0200

    feat: Implement TP03 concerning variables and debug

---
+++ src/main/java/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte myByteVariable = 1;
+		short myShortVariable = 2;
+		int myIntVariable = 3;
+		long myLongVariable = 1L;
+		float myFloatVariable = 0.3f;
+		double myDoubleVariable = 0.4;
+		char myCharvariable = 'f';
+		boolean myBooleanVariable = true;
+		String myStringVariable = "ProjectDivinity";
+		
+		System.out.println(myStringVariable);
+		
+		String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+		
+		System.out.println(randomString);
+		
+	}
+
+}
